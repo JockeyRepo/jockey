3e6f439057b69c315dead110c096699211c2d9f0
BelierJavier <BelierJavier@The-Terminator.local>
2018-10-12 19:01:29 -0400

    Current Phase
    Changed fill in text to Description.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp'),
   autoprefixer = require('gulp-autoprefixer'),
   sourcemaps = require('gulp-sourcemaps'),
   connect = require('gulp-connect-php'),
-  sass = require('gulp-ruby-sass'),
   browserSync = require('browser-sync');
 
 var output = './assets/css';
@@ -15,19 +14,6 @@ gulp.task('default', function() {
 });
 
 
-gulp.task('styles', function () {
-  return sass('assets/sass/style.scss', { style: 'compressed', sourcemap:true })
-    .pipe(sourcemaps.init())
-    .pipe(autoprefixer(autoprefixerOptions))
-    .pipe(sourcemaps.write())
-    .pipe(sourcemaps.write('maps', {
-      includeContent: false,
-      sourceRoot: 'source'
-    }))
-    .pipe(gulp.dest(output))
-    .pipe(browserSync.stream({ match: '**/*.css' }));
-});
-
 gulp.task('connect-sync', function () {
   connect.server({}, function () {
     browserSync({
@@ -37,10 +23,9 @@ gulp.task('connect-sync', function () {
 });
 
 gulp.task('watch', function () {
-  gulp.watch("sass/**", ['styles']);
   gulp.watch(['js/**/*.js', '*.html', '**/*.php']).on('change', browserSync.reload);
 });
 
-gulp.task('default', ['styles', 'connect-sync'], function () {
+gulp.task('default', ['connect-sync'], function () {
   gulp.start('watch');
 });
